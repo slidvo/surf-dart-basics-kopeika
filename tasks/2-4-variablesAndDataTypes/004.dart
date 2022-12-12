ff9334da0f04f3606e8dfb24bd1bb769371c43d0
Vyacheslav <ks031088@gmail.com>
2022-12-12 18:22:21 +0300

    task: variables and data types

---
+++ tasks/2-4-variablesAndDataTypes/004.dart
@@ -0,0 +1,18 @@
+/**
+ * Создать переменную  с именем dyn при помощи dynamic, 
+ * проинициализировать его целочисленным значением, далее попытаться присвоить переменной строковое значение переменной text. 
+ * Каков результат? Почему?
+ */
+
+void main() {
+  dynamic dyn = 42;
+  var text = "Text example";
+  
+  print("dyn type is : ${dyn.runtimeType}");
+  // dyn type is : int
+
+  dyn = text;
+
+  print("dyn type is : ${dyn.runtimeType}");
+  // dyn type is : String
+}
