a8914edd2766e60c6950ce63812ab906e0d12c2d
Vyacheslav <ks031088@gmail.com>
2022-12-13 17:08:35 +0300

    init commit

---
+++ bin/surf_flutter_course_kopeika.dart
@@ -0,0 +1,5 @@
+import 'package:surf_flutter_course_kopeika/surf_flutter_course_kopeika.dart' as surf_flutter_course_kopeika;
+
+void main(List<String> arguments) {
+  print('Hello world: ${surf_flutter_course_kopeika.calculate()}!');
+}
---
+++ test/surf_flutter_course_kopeika_test.dart
@@ -0,0 +1,8 @@
+import 'package:surf_flutter_course_kopeika/surf_flutter_course_kopeika.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
