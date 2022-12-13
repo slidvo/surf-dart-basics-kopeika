093ca6b2b1fc23c0018922c3145dbfc2fd3ea92e
Vyacheslav <ks031088@gmail.com>
2022-12-13 19:18:53 +0300

    task: 2-5 Управляющие конструкции Dart

--- tasks/2-5-controlStructures/002.dart
+++ tasks/2-5-controlStructures/002.dart
@@ -1,3 +1,12 @@
 /**
  * Используя циклы, напишите программу, которая выводит на консоль все четные числа от 0 до 100.
  */
+main() {
+  Set<int> setNumbers = {};
+  for (int i = 0; i <= 100; i++) {
+    if (i.isEven) {
+      setNumbers.add(i);
+    }
+  }
+  print(setNumbers);
+}
--- tasks/2-5-controlStructures/003.dart
+++ tasks/2-5-controlStructures/003.dart
@@ -1,5 +1,36 @@
+import 'dart:io';
+
 /**Написать программу, которая слушает ввод в консоли,
  * складывает вводимые пользователем числа. Если пользователь ввел stop, завершить приложение.
- * Если пользователь вводит некорректные данные - прервать текущую итерацию, начать заново. 
- * 
-*/
+ * Если пользователь вводит некорректные данные - прервать текущую итерацию, начать заново.
+ *
+ */
+void main() {
+  while (true) {
+    try {
+      print("Введите первое число");
+      String s1 = stdin.readLineSync() ?? '0';
+      if (s1 == "stop") break;
+      int x = int.parse(s1);
+      print("Введите второе число");
+      String s2 = stdin.readLineSync() ?? '0';
+      if (s2 == "stop") break;
+      int y = int.parse(s2);
+      int result = x + y;
+      print("Result: $result");
+      print("Попробовать ещё раз? (Y/N): ");
+      String tryAgain = (stdin.readLineSync() ?? "N").toLowerCase();
+      if (tryAgain.compareTo("y") == 0) {
+        continue;
+      } else if (tryAgain.compareTo("n") == 0 ||
+          tryAgain.compareTo("stop") == 0) {
+        break;
+      } else {
+        throw FormatException;
+      }
+    } catch (e) {
+      print("Введены некорректные данные. Попробуйте снова");
+      continue;
+    }
+  }
+}
