616f09993a11869097db6248552137117b15acfa
Vyacheslav <ks031088@gmail.com>
2022-12-17 05:32:26 +0300

    task: 2-6 Функции. Внесение правок в ПР 4

--- tasks/2-6-functions/001.dart
+++ tasks/2-6-functions/001.dart
@@ -9,8 +9,6 @@ void main() {
   print(reverseTextOrder(a));
 }
 
-String reverseTextOrder(String text){
-  var splitText = text.split(" ");
-  splitText.sort((a, b) => b.compareTo(a));
-  return splitText.reduce((value, element) => "$value $element");
+String reverseTextOrder(String text) {
+  return text.split(" ").reversed.join(" ");
 }
